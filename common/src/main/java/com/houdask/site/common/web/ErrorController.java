3ffe451f636c2147a89934df6c6ccf64cd8b6c70
Halburt <junhui1213@126.com>
2018-01-19 18:01:31 +0800

    完成web springboot:run 支持jsp redis缓存

--- common/src/main/java/com/houdask/site/common/web/ErrorController.java
+++ common/src/main/java/com/houdask/site/common/web/ErrorController.java
@@ -3,7 +3,7 @@ package com.houdask.site.common.web;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+//@RestController
 public class ErrorController {
 
     public String error(){
