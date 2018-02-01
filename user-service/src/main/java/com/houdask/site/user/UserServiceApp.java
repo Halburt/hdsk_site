0b53684e4f3654de94085409b483542819b42488
Halburt <junhui1213@126.com>
2018-02-01 12:02:16 +0800

    添加HdSpringBootService快速注解

--- user-service/src/main/java/com/houdask/site/user/UserServiceApp.java
+++ user-service/src/main/java/com/houdask/site/user/UserServiceApp.java
@@ -2,16 +2,7 @@ package com.houdask.site.user;
 
 import com.houdask.site.common.annotation.HdSpringBootService;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cache.CacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.util.Arrays;
-@Configuration
 @HdSpringBootService("com.houdask.site.user.dao")
 public class UserServiceApp {
     public static void main(String[] args) {
