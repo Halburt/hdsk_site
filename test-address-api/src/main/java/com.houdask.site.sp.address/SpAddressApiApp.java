7c1dc3b2e90ea3d3d60391e0a32e47a87420b68a
Halburt <junhui1213@126.com>
2018-02-08 13:58:05 +0800

    优化

---
+++ test-address-api/src/main/java/com.houdask.site.sp.address/SpAddressApiApp.java
@@ -0,0 +1,11 @@
+package com.houdask.site.sp.address;
+
+import com.houdask.site.common.annotation.HdSpringBootApi;
+import org.springframework.boot.SpringApplication;
+
+@HdSpringBootApi
+public class SpAddressApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpAddressApiApp.class, args);
+    }
+}
